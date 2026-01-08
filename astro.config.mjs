ee355b62f024ba98a7d99f11a23d3184517015d0
Brad Azevedo <brad2k@gmail.com>
2026-01-08 14:51:00 -0800

    Initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,31 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from "astro/config";
 
-import netlify from '@astrojs/netlify';
+import netlify from "@astrojs/netlify";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: netlify()
+  adapter: netlify(),
+
+  experimental: {
+    fonts: [
+      {
+        provider: fontProviders.google(),
+        name: "Playfair Display",
+        cssVariable: "--font-Playfair",
+        subsets: ["latin"],
+      },
+      {
+        provider: fontProviders.google(),
+        name: "Montserrat",
+        cssVariable: "--font-Montserrat",
+        weights: ["400 700"],
+        subsets: ["latin"],
+      },
+    ],
+  },
+
+  integrations: [react()],
 });
